00bc9e5d19ccad9b3627d75000c9090dd2af540b
Savodyarkin Nikita <snikitafut@mail.ru>
2023-12-11 17:09:18 +0300

    string to lower

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+
+
+string StringToLower(string str) {
+    string res = str.ToLower();
+    return res;
+}
+
+string str = "SgffSRfrSrGG";
+string res = StringToLower(str);
+Console.WriteLine(res);
